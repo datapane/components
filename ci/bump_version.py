3294913a548a63e5056a99e50a3daa0792e7158d
Anthony King <anthony@datapane.com>
2023-03-15 15:59:33 +0000

    setup a task for doing the git commit + tag of a bump

--- ci/bump_version.py
+++ ci/bump_version.py
@@ -13,9 +13,20 @@ def _get_new_version():
     return f"{today.year}.{today.month}.{today.day}"
 
 
-def main():
+def _commit_and_tag(new_version: str):
+    prefixed_version = f"v{new_version}"
+
+    sh.git.add("pyproject.toml")
+    sh.git.commit("-m", f"Release {prefixed_version}")
+    sh.git.tag("-a", f"{prefixed_version}", "-m", f"Release {prefixed_version}")
+
+
+def main(git: bool = False):
     new_version = _get_new_version()
     sh.poetry.version("-vv", "--", new_version, _fg=True)
+
+    if git:
+        _commit_and_tag(new_version)
 
 
 if __name__ == "__main__":
